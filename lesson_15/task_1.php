6735f9352ceeff140da2f3d426b418f61596a026
PavelZubarev <213633@inbox.ru>
2022-12-13 19:01:47 +0300

    'd'

--- lesson_15/task_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$testString= readline("введите числа через пробел:");
-$pattern="/\+\d{3}\s\(|29|25|33|\)\s\d{3}\s\d{2}\s\d{2}/";
-
-if(preg_match($pattern, $testString)) {
-    echo 'номер задан верно' . PHP_EOL;
- } else {
-    echo 'Ошибка: не соответствует формату!' . PHP_EOL;
- }
