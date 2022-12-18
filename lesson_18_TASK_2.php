bade212b7bdbea5a6052a56f184cc2bac60d50b1
PavelZubarev <213633@inbox.ru>
2022-12-18 20:42:54 +0300

    down

--- lesson_18_TASK_2.php
+++ lesson_18_TASK_2.php
@@ -9,7 +9,7 @@
 if(!empty($_GET['data'])){
     setcookie('daata',$_GET['data']);
     $_COOKIE['daata']= explode('-', $_GET['data']);
-    $date_x= explode('.', date('Y.m.d'));
+    $date_x= explode('-', date('Y-m-d'));
     echo'<pre>';
     echo print_r($_COOKIE['daata']) . '<br>';
     echo print_r($date_x) . '<br>';
@@ -22,11 +22,11 @@ if(!empty($_GET['data'])){
         echo $_COOKIE['text'] . '<br>';
     }
     else{
-        $data_z=mktime(0, 0, 0,  $_COOKIE['daata'][2],  $_COOKIE['daata'][1], date('Y'));
-        echo $data_z . '<br>' . $date_x;
+           $data_z=mktime(0, 0, 0,  $_COOKIE['daata'][2],  $_COOKIE['daata'][1], date('Y'));
+           echo $data_z . '<br>' . $date_x;
 
           if($data_z< $data_B){
-          $data_z=mktime(0, 0, 0,  $_COOKIE['daata'][2],  $_COOKIE['daata'][1], (date('Y')+1));
+              $data_z=mktime(0, 0, 0,  $_COOKIE['daata'][2],  $_COOKIE['daata'][1], (date('Y')+1));
              } 
         
              echo 'до дня рождения ' . floor(($data_z-$data_B)/86400) . ' Дней';
